8446c8723ca3303fa3c6415bffc4f88437faed02
Kavia AI <production@kavia.ai>
2025-07-30 04:10:03 +0000

    CheckPoint - cg9c31e08d

--- material_ui_library_frontend/projects/material-blue-lib/src/public-api.ts
+++ material_ui_library_frontend/projects/material-blue-lib/src/public-api.ts
@@ -4,4 +4,5 @@ export * from './lib/text-input-with-icon/text-input-with-icon.component';
 export * from './lib/primary-button/primary-button.component';
 export * from './lib/link-text/link-text.component';
 export * from './lib/background-ellipses/background-ellipses.component';
+export * from './lib/login-page-layout/login-page-layout.component';
 // TODO: Export legacy MaterialBlueLibModule and wrappers if still needed
